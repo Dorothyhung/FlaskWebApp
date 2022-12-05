addd1510761a791b70a307bd56457c8d0baa6cce
Dorothyhung <dorothyh0602@gmail.com>
2022-12-04 22:51:48 -0800

    change

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,18 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from os import path
+#from flask_sqlalchemy import SQLAlchemy
+#from os import path
 
 #create database
-db = SQLAlchemy()
-DB_NAME = "database.db"
+#db = SQLAlchemy()
+#DB_NAME = "database.db"
 
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'wow'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    #db.init_app(app)
 
     from .views import views
     from .resume import resume
