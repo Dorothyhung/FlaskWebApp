8d8858674c21d3ec3f4e37ee09baf30e3d72d218
Dorothyhung <dorothyh0602@gmail.com>
2022-12-20 09:25:01 -0800

    Connect to postgres

--- website/__init__.py
+++ website/__init__.py
@@ -4,19 +4,19 @@ from flask import Flask
 #import pyodbc as odbc
 #import pandas as pd
 #import sqlalchemy
-#import psycopg2
-#import urllib.parse as up
+import psycopg2
+import urllib.parse as up
 
 
 
-"""url = up.urlparse('postgres://nftfhbsb:ys0LGtiq29f4zFG6Bqnelk7d20VAySV2@mahmud.db.elephantsql.com/nftfhbsb')
+url = up.urlparse('postgres://nftfhbsb:ys0LGtiq29f4zFG6Bqnelk7d20VAySV2@mahmud.db.elephantsql.com/nftfhbsb')
 conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname, port=url.port )
 cursor  = conn.cursor()
 #cursor.execute('static\database.sql')
-conn.commit()
+#conn.commit()
 print('Table created')
 
-
+"""
 connstring = ("Driver={SQL Server Native Client 11.0};"
             "Server=USXXX00345,67800;"
             "Database=DB02;"
